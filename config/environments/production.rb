ad1c71dc54db8652da4444ce2fa5008a2c5a1c52
atsushi0919 <hatake0919@yahoo.co.jp>
2021-05-14 09:23:59 +0900

    update product setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,18 +62,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  host = "infinite-dusk-58165.herokuapp.com"
+  host = "pure-reaches-65875.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :port => 587,
-    :address => "smtp.sendgrid.net",
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password => ENV["SENDGRID_PASSWORD"],
+    :port => ENV["MAILGUN_SMTP_PORT"],
+    :address => ENV["MAILGUN_SMTP_SERVER"],
+    :user_name => ENV["MAILGUN_SMTP_LOGIN"],
+    :password => ENV["MAILGUN_SMTP_PASSWORD"],
     :domain => host,
     :authentication => :plain,
-    :enable_starttls_auto => true,
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
